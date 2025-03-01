ad85f3bbdaa7275835a355588a1a3802413c0780
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2025-03-01 17:44:11 +0200

    chore: setup `cors` and `helmet`

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 dotenv.config();
+import cors from 'cors';
+import helmet from 'helmet';
 import connectToDB from './config/db.js';
 
 const app = express();
 
 // Connect to the database
 connectToDB();
+
+// Helmet
+app.use(helmet());
+
+// Cors Policy
+app.use(cors());
 
 /* eslint no-undef: off */
 app.listen(process.env.PORT, () => {
