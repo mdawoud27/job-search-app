73d74b8bdd0b071f70081fe68b603b5ddab32ed5
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2025-03-05 12:44:46 +0200

    chore: setup `passportjs` middleware

--- src/index.js
+++ src/index.js
@@ -25,6 +25,22 @@ app.use(
   }),
 );
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(
+  new Strategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+    },
+    (accessToken, refreshToken, profile, done) => {
+      return done(null, profile);
+    },
+  ),
+);
+
 // Apply rate limiter to all requests
 app.use(apiLimiter);
 
