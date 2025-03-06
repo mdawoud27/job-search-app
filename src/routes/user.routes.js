68e5790ad271e8ecb8cfaf8ebaf687d7215b5308
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2025-03-06 13:23:05 +0200

    feat: add user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,9 @@
 import { Router } from 'express';
+import { verifyUserPermission } from '../middlewares/verifyUserPermission';
+import { updateUserAccount } from '../controllers/user.controller';
 
 const router = Router();
+
+router.put('/api/user/:userId', verifyUserPermission, updateUserAccount);
 
 export default router;
