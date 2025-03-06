ca1948aa43c0d68f55f8a90c55ee1b02b10710d6
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2025-03-06 13:27:31 +0200

    feat: add verify access token middleware

---
+++ src/middlewares/verifyAccessToken.js
@@ -0,0 +1,23 @@
+import jwt from 'jsonwebtoken';
+
+export const verifyAccessToken = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ message: 'No token provided' });
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  try {
+    /* eslint no-undef: off */
+    const decoded = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    if (error.name === 'TokenExpiredError') {
+      return res.status(401).json({ message: 'Token expired' });
+    }
+    return res.status(403).json({ message: 'Invalid token' });
+  }
+};
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,17 @@
 import { Router } from 'express';
 import { verifyUserPermission } from '../middlewares/verifyUserPermission';
 import { updateUserAccount } from '../controllers/user.controller';
+import { verifyAccessToken } from '../middlewares/verifyAccessToken';
 
 const router = Router();
 
-router.put('/api/user/:userId', verifyUserPermission, updateUserAccount);
+router.put('/user/update', verifyAccessToken, updateUserAccount);
+
+router.put(
+  '/api/user/:userId',
+  verifyAccessToken,
+  verifyUserPermission,
+  updateUserAccount,
+);
 
 export default router;
