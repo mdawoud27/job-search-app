cd3b068429340b6b87d66f6f20c2d659dd368097
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2025-03-07 21:10:52 +0200

    refactor: admin routes

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,16 +1,27 @@
 import { Router } from 'express';
 import { verifyAccessToken } from '../middlewares/auth.js';
 import { verifyAdminPermission } from '../middlewares/verifyAdminPermission.js';
-import { banOrUnbanUser } from '../controllers/admin.controller.js';
+import {
+  banOrUnbanCompany,
+  banOrUnbanUser,
+} from '../controllers/admin.controller.js';
 
 const router = Router();
 
 // Ban or unbanned specific user
 router.patch(
-  '/admin/ban',
+  '/admin/user/ban',
   verifyAccessToken,
   verifyAdminPermission,
   banOrUnbanUser,
+);
+
+// Ban or unbanned specific company
+router.patch(
+  '/admin/company/ban',
+  verifyAccessToken,
+  verifyAdminPermission,
+  banOrUnbanCompany,
 );
 
 export default router;
