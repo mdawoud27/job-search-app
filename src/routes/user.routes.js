dc921ac41b9c29194f07e6fbe9b4e9a23d72637d
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2025-03-19 16:56:46 +0200

    refactor: user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -12,7 +12,6 @@ import {
 import { verifyAccessToken } from '../middlewares/auth.js';
 import { verifyUserPermission } from '../middlewares/verifyUserPermission.js';
 import { upload } from '../utils/imageStorage.js';
-import { apiLimiter } from '../utils/apiLimiter.js';
 
 const router = Router();
 
@@ -21,27 +20,20 @@ router.put(
   '/user/:id',
   verifyAccessToken,
   verifyUserPermission,
-  apiLimiter,
   updateUserAccount,
 );
 
 // Retrive user profile date
-router.get('/user/profile', verifyAccessToken, apiLimiter, getUserProfile);
+router.get('/user/profile', verifyAccessToken, getUserProfile);
 
 // Update user password
-router.patch(
-  '/user/profile/password',
-  verifyAccessToken,
-  apiLimiter,
-  updateUserPassword,
-);
+router.patch('/user/profile/password', verifyAccessToken, updateUserPassword);
 
 // Upload profile picture
 router.post(
   '/user/profile/profile-pic',
   verifyAccessToken,
   upload.single('profilePic'),
-  apiLimiter,
   uploadProfilePic,
 );
 
@@ -50,27 +42,16 @@ router.post(
   '/user/profile/cover-pic',
   verifyAccessToken,
   upload.single('coverPic'),
-  apiLimiter,
   uploadCoverPic,
 );
 
 // Delete profile picture
-router.delete(
-  '/user/profile/profile-pic',
-  verifyAccessToken,
-  apiLimiter,
-  deleteProfilePic,
-);
+router.delete('/user/profile/profile-pic', verifyAccessToken, deleteProfilePic);
 
 // Delete cover picture
-router.delete(
-  '/user/profile/cover-pic',
-  verifyAccessToken,
-  apiLimiter,
-  deleteCoverPic,
-);
+router.delete('/user/profile/cover-pic', verifyAccessToken, deleteCoverPic);
 
 // Soft delete user account
-router.delete('/user/delete', verifyAccessToken, apiLimiter, softDeleteAccount);
+router.delete('/user/delete', verifyAccessToken, softDeleteAccount);
 
 export default router;
