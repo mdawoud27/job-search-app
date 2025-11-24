81ee6d22fb96ca8b70700817c400df071018a09c
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2025-11-24 11:18:19 +0200

    feat: add token utils

---
+++ src/utils/tokens.utils.js
@@ -0,0 +1,20 @@
+import jwt from 'jsonwebtoken';
+import * as config from 'dotenv';
+config.config();
+
+/* eslint no-undef: off */
+export class TokenUtils {
+  static genAccessToken(user) {
+    return jwt.sign(
+      { id: user._id, email: user.email, role: user.role },
+      process.env.JWT_ACCESS_SECRET,
+      { expiresIn: '1h' },
+    );
+  }
+
+  static genRefreshToken(user) {
+    return jwt.sign({ id: user._id }, process.env.JWT_REFRESH_SECRET, {
+      expiresIn: '7d',
+    });
+  }
+}
