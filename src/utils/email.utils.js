6f8be70cfd8bc06ff8b358597668cf875ab08298
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2025-11-25 18:07:54 +0200

    refactor: add email utils

---
+++ src/utils/email.utils.js
@@ -0,0 +1,30 @@
+import nodemailer from 'nodemailer';
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.USER_EMAIL,
+    pass: process.env.USER_PASS,
+  },
+});
+
+/* eslint no-undef: off */
+export const sendOTPEmail = async (email, otp, subject = '') => {
+  await transporter.sendMail({
+    from: process.env.USER_EMAIL,
+    to: email,
+    subject: subject || 'Verify your Email',
+    text: `Your OTP for email verification is: ${otp}. It will expire in 10 minutes.`,
+  });
+};
+
+export const sendEmail = async ({ to, subject, html }) => {
+  const mailOptions = {
+    from: process.env.USER_EMAIL,
+    to,
+    subject,
+    html,
+  };
+
+  return transporter.sendMail(mailOptions);
+};
