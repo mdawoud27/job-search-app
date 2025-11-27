56bb3f45201d59da912f50f126c349565b59a276
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2025-11-27 19:01:20 +0200

    feat: container file

--- src/container.js
+++ src/container.js
@@ -4,22 +4,36 @@ import { AuthController } from './controllers/auth.controller.js';
 
 import { UserService } from './services/user.service.js';
 import { UserController } from './controllers/user.controller.js';
+import { AdminService } from './services/admin.service.js';
+import { AdminController } from './controllers/admin.controller.js';
+import { AdminDao } from './daos/admin.dao.js';
 
 // Repositories
 const userRepository = new UserDAO();
+const adminRepository = new AdminDao();
 
 // Services
 const authService = new AuthService(userRepository);
 const userService = new UserService(userRepository);
+const adminService = new AdminService(adminRepository);
 
 // Controllers
 const authController = new AuthController(authService);
 const userController = new UserController(userService);
+const adminController = new AdminController(adminService);
 
 export {
+  // daos
   userRepository,
+  adminRepository,
+
+  // services
   authService,
   userService,
+  adminService,
+
+  // controllers
   authController,
   userController,
+  adminController,
 };
