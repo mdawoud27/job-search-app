203efe70f4928a84d0e3c93dbef5a5c13ab6b781
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2025-11-28 19:00:59 +0200

    feat: inject admin controller and services

--- src/container.js
+++ src/container.js
@@ -1,12 +1,14 @@
 import { UserDAO } from './daos/user.dao.js';
+import { AdminDao } from './daos/admin.dao.js';
+
 import { AuthService } from './services/auth.service.js';
 import { AuthController } from './controllers/auth.controller.js';
 
 import { UserService } from './services/user.service.js';
 import { UserController } from './controllers/user.controller.js';
+
 import { AdminService } from './services/admin.service.js';
 import { AdminController } from './controllers/admin.controller.js';
-import { AdminDao } from './daos/admin.dao.js';
 
 // Repositories
 const userRepository = new UserDAO();
@@ -15,7 +17,7 @@ const adminRepository = new AdminDao();
 // Services
 const authService = new AuthService(userRepository);
 const userService = new UserService(userRepository);
-const adminService = new AdminService(adminRepository);
+const adminService = new AdminService(userRepository, adminRepository);
 
 // Controllers
 const authController = new AuthController(authService);
