bb1434372728bbcbbbbe8eff1f35b40932fb1922
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2025-11-29 10:37:47 +0200

    fix: admin api design

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -5,35 +5,35 @@ import { Authorization } from '../middlewares/auth.middleware.js';
 const router = Router();
 
 router.patch(
-  '/api/admin/ban-user',
+  '/admin/ban-user',
   Authorization.verifyToken,
   Authorization.verifyAdminPermission,
   (req, res, next) => adminController.banUser(req, res, next),
 );
 
 router.patch(
-  '/api/admin/unban-user',
+  '/admin/unban-user',
   Authorization.verifyToken,
   Authorization.verifyAdminPermission,
   (req, res, next) => adminController.unbanUser(req, res, next),
 );
 
 router.patch(
-  '/api/admin/ban-company',
+  '/admin/ban-company',
   Authorization.verifyToken,
   Authorization.verifyAdminPermission,
   (req, res, next) => adminController.banCompany(req, res, next),
 );
 
 router.patch(
-  '/api/admin/unban-company',
+  '/admin/unban-company',
   Authorization.verifyToken,
   Authorization.verifyAdminPermission,
   (req, res, next) => adminController.unbanCompany(req, res, next),
 );
 
 router.patch(
-  '/api/admin/approve-company',
+  '/admin/approve-company',
   Authorization.verifyToken,
   Authorization.verifyAdminPermission,
   (req, res, next) => adminController.approveCompany(req, res, next),
