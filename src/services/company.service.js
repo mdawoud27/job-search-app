45769d4ece9541074cc8ebe769ceeb6cb7ca054b
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2025-11-29 19:40:14 +0200

    feat: add update company service

--- src/services/company.service.js
+++ src/services/company.service.js
@@ -25,4 +25,31 @@ export class CompanyService {
       throw error;
     }
   }
+
+  async updateCompany(companyId, dto, userId) {
+    try {
+      const user = await this.userDao.findByIdAndActive(userId);
+      const company = await this.companyDao.update(companyId, dto, userId);
+
+      if (!company) {
+        throw new Error('Company not found');
+      }
+      if (company.deletedAt || company.bannedAt) {
+        throw new Error('Company is deleted or banned');
+      }
+      return {
+        message: 'Company updated successfully',
+        createdBy: user.email,
+        role: user.role,
+        data: CompanyResponseDto.toResponse(company),
+      };
+    } catch (error) {
+      if (error.code === 11000) {
+        // duplicate key error
+        const field = Object.keys(error.keyPattern)[0];
+        throw new Error(`${field} already exists`);
+      }
+      throw error;
+    }
+  }
 }
