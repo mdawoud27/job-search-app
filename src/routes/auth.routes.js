30fe05e8b26da1aa6493d06e33e9a651dd09d956
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2025-11-29 10:36:06 +0200

    fix: auth api design

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -4,40 +4,40 @@ import { authController } from '../container.js';
 
 const router = Router();
 
-router.post('/api/auth/signup', apiLimiter, (req, res, next) =>
+router.post('/auth/signup', apiLimiter, (req, res, next) =>
   authController.signup(req, res, next),
 );
 
-router.post('/api/auth/confirm-otp', apiLimiter, (req, res, next) =>
+router.post('/auth/confirm-otp', apiLimiter, (req, res, next) =>
   authController.confirm(req, res, next),
 );
 
-router.post('/api/auth/resend-otp', apiLimiter, (req, res, next) =>
+router.post('/auth/resend-otp', apiLimiter, (req, res, next) =>
   authController.resentOTP(req, res, next),
 );
 
-router.post('/api/auth/signin', apiLimiter, (req, res, next) =>
+router.post('/auth/signin', apiLimiter, (req, res, next) =>
   authController.login(req, res, next),
 );
 
-router.post('/api/auth/forget-password', apiLimiter, (req, res, next) =>
+router.post('/auth/forget-password', apiLimiter, (req, res, next) =>
   authController.forgotPassword(req, res, next),
 );
 
-router.post('/api/auth/reset-password', apiLimiter, (req, res, next) =>
+router.post('/auth/reset-password', apiLimiter, (req, res, next) =>
   authController.resetPassword(req, res, next),
 );
 
-router.post('/api/auth/refresh-token', apiLimiter, (req, res, next) =>
+router.post('/auth/refresh-token', apiLimiter, (req, res, next) =>
   authController.refreshToken(req, res, next),
 );
 
 // Google OAuth routes
-router.get('/api/auth/google', (req, res, next) =>
+router.get('/auth/google', (req, res, next) =>
   authController.googleAuth(req, res, next),
 );
 
-router.get('/api/auth/google/callback', (req, res, next) =>
+router.get('/auth/google/callback', (req, res, next) =>
   authController.googleCallback(req, res, next),
 );
 export default router;
