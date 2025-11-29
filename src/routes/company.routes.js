320219cb895d027e52f8d7638a39b34034a82dae
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2025-11-29 15:10:00 +0200

    refactor: company routes

--- src/routes/company.routes.js
+++ src/routes/company.routes.js
@@ -1,99 +1,18 @@
 import { Router } from 'express';
-import { verifyAccessToken } from '../middlewares/auth.js';
-import { verifyAdminPermission } from '../middlewares/verifyAdminPermission.js';
-import {
-  addCompany,
-  deleteCompanyCoverPic,
-  deleteCompanyLogo,
-  getCompanyWithJobs,
-  searchCompaniesByName,
-  softDeleteCompany,
-  updateCompany,
-  uploadCompanyCoverPic,
-  uploadCompanyLogo,
-} from '../controllers/company.controller.js';
-import { upload } from '../utils/imageStorage.js';
+import { Authorization } from '../middlewares/auth.middleware.js';
 import { apiLimiter } from '../utils/apiLimiter.js';
+import { companyController } from '../container.js';
 
 const router = Router();
 
-// Add company
 router.post(
-  '/api/company',
+  '/company/create',
   apiLimiter,
-  verifyAccessToken,
-  verifyAdminPermission,
-  addCompany,
-);
-
-// Update company
-router.put(
-  '/api/company/:companyId',
-  apiLimiter,
-  verifyAccessToken,
-  verifyAdminPermission,
-  updateCompany,
-);
-
-// Soft delete company
-router.delete(
-  '/api/company/:companyId',
-  apiLimiter,
-  verifyAccessToken,
-  verifyAdminPermission,
-  softDeleteCompany,
-);
-
-// Get company with jobs
-router.get(
-  '/api/company/:companyId',
-  apiLimiter,
-  verifyAccessToken,
-  verifyAdminPermission,
-  getCompanyWithJobs,
-);
-
-// Search for company by name
-router.get(
-  '/api/company/',
-  apiLimiter,
-  verifyAccessToken,
-  verifyAdminPermission,
-  searchCompaniesByName,
-);
-
-// Upload company logo
-router.post(
-  '/api/company/:companyId/logo',
-  apiLimiter,
-  verifyAccessToken,
-  upload.single('image'),
-  uploadCompanyLogo,
-);
-
-// Upload company cover pic
-router.post(
-  '/api/company/:companyId/cover-pic',
-  apiLimiter,
-  verifyAccessToken,
-  upload.single('image'),
-  uploadCompanyCoverPic,
-);
-
-// Delete company logo
-router.delete(
-  '/api/company/:companyId/logo',
-  apiLimiter,
-  verifyAccessToken,
-  deleteCompanyLogo,
-);
-
-// Delete company cover pic
-router.delete(
-  '/api/company/:companyId/cover-pic',
-  apiLimiter,
-  verifyAccessToken,
-  deleteCompanyCoverPic,
+  Authorization.verifyToken,
+  Authorization.verifyHRPermission,
+  (req, res, next) => {
+    companyController.createCompany(req, res, next);
+  },
 );
 
 export default router;
