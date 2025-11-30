c6534c7da88c187059a5e5d32fb13491ffd746b9
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2025-11-30 13:49:06 +0200

    feat: add company images services

--- src/services/company.service.js
+++ src/services/company.service.js
@@ -1,4 +1,5 @@
 import { CompanyResponseDto } from '../dtos/company/company-response.dto.js';
+import { CloudinaryUtils } from '../utils/cloudinary.util.js';
 
 export class CompanyService {
   constructor(userDao, companyDao) {
@@ -102,6 +103,104 @@ export class CompanyService {
           jobs: company.jobs,
         };
       }),
+    };
+  }
+
+  async uploadCompanyLogo(companyId, logo) {
+    const company = await this.companyDao.isActive(companyId);
+    if (!company) {
+      const error = new Error('Company not found or deleted or banned');
+      error.statusCode = 404;
+      throw error;
+    }
+
+    // Delete old image
+    if (company.logo?.public_id) {
+      await CloudinaryUtils.deleteCloudinaryFile(company.logo.public_id);
+    }
+
+    const updatedCompany = await this.companyDao.updateCompanyLogo(companyId, {
+      secure_url: logo.secure_url,
+      public_id: logo.public_id,
+    });
+
+    return {
+      message: 'Logo uploaded successfully',
+      data: {
+        logo: updatedCompany.logo,
+      },
+    };
+  }
+
+  async deleteCompanyLogo(companyId) {
+    const company = await this.companyDao.isActive(companyId);
+    if (!company) {
+      throw new Error('Company not found or deleted or banned');
+    }
+
+    if (company.logo?.public_id) {
+      await CloudinaryUtils.deleteCloudinaryFile(company.logo.public_id);
+      company.logo = null;
+      await company.save();
+      return {
+        message: 'Logo deleted successfully',
+        data: {
+          logo: company.logo,
+        },
+      };
+    }
+    return {
+      message: 'No logo to delete',
+      data: {
+        logo: company.logo,
+      },
+    };
+  }
+
+  async uploadCompanyCover(companyId, cover) {
+    const company = await this.companyDao.isActive(companyId);
+    if (!company) {
+      throw new Error('Company not found or deleted or banned');
+    }
+
+    // Delete old image
+    if (company.coverPic?.public_id) {
+      await CloudinaryUtils.deleteCloudinaryFile(company.coverPic.public_id);
+    }
+    const updatedCompany = await this.companyDao.updateCompanyCover(companyId, {
+      secure_url: cover.secure_url,
+      public_id: cover.public_id,
+    });
+    return {
+      message: 'Cover uploaded successfully',
+      data: {
+        coverPic: updatedCompany.coverPic,
+      },
+    };
+  }
+
+  async deleteCompanyCover(companyId) {
+    const company = await this.companyDao.isActive(companyId);
+    if (!company) {
+      throw new Error('Company not found or deleted or banned');
+    }
+
+    if (company.coverPic?.public_id) {
+      await CloudinaryUtils.deleteCloudinaryFile(company.coverPic.public_id);
+      company.coverPic = null;
+      await company.save();
+      return {
+        message: 'Cover deleted successfully',
+        data: {
+          coverPic: company.coverPic,
+        },
+      };
+    }
+    return {
+      message: 'No cover to delete',
+      data: {
+        coverPic: company.coverPic,
+      },
     };
   }
 }
