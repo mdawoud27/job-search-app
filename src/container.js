d0a9bf1f5f4985e6ee168fe1ce7d148c84a70602
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2025-12-01 12:38:23 +0200

    feat: integrate job dependencies into container

--- src/container.js
+++ src/container.js
@@ -1,6 +1,7 @@
 import { UserDAO } from './daos/user.dao.js';
 import { AdminDao } from './daos/admin.dao.js';
 import { CompanyDAO } from './daos/company.dao.js';
+import { JobDao } from './daos/job.dao.js';
 
 import { AuthService } from './services/auth.service.js';
 import { AuthController } from './controllers/auth.controller.js';
@@ -14,10 +15,14 @@ import { AdminController } from './controllers/admin.controller.js';
 import { CompanyService } from './services/company.service.js';
 import { CompanyController } from './controllers/company.controller.js';
 
+import { JobService } from './services/job.service.js';
+import { JobController } from './controllers/job.controller.js';
+
 // Repositories
 const userRepository = new UserDAO();
 const adminRepository = new AdminDao();
 const companyRepository = new CompanyDAO();
+const jobRepository = new JobDao();
 
 // Services
 const authService = new AuthService(userRepository);
@@ -28,28 +33,37 @@ const adminService = new AdminService(
   companyRepository,
 );
 const companyService = new CompanyService(userRepository, companyRepository);
+const jobService = new JobService(
+  userRepository,
+  companyRepository,
+  jobRepository,
+);
 
 // Controllers
 const authController = new AuthController(authService);
 const userController = new UserController(userService);
 const adminController = new AdminController(adminService);
 const companyController = new CompanyController(companyService);
+const jobController = new JobController(jobService);
 
 export {
   // daos
   userRepository,
   adminRepository,
   companyRepository,
+  jobRepository,
 
   // services
   authService,
   userService,
   adminService,
   companyService,
+  jobService,
 
   // controllers
   authController,
   userController,
   adminController,
   companyController,
+  jobController,
 };
