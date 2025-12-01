9deb26583d5106344b0cb4a135f4c8afe486d3b2
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2025-12-01 13:19:03 +0200

    feat: add update job controller endpoint

--- src/controllers/job.controller.js
+++ src/controllers/job.controller.js
@@ -1,4 +1,5 @@
 import { CreateJobDto } from '../dtos/job/create-job.dto.js';
+import { UpdateJobDto } from '../dtos/job/update-job.dto.js';
 
 export class JobController {
   constructor(jobService) {
@@ -15,6 +16,24 @@ export class JobController {
         value,
         req.user.id,
         req.params.companyId,
+      );
+      res.status(201).json(job);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async updateJob(req, res, next) {
+    try {
+      const { error, value } = UpdateJobDto.validate(req.body);
+      if (error) {
+        return res.status(400).json({ message: error.details[0].message });
+      }
+      const job = await this.jobService.updateJob(
+        value,
+        req.user.id,
+        req.params.companyId,
+        req.params.jobId,
       );
       res.status(201).json(job);
     } catch (error) {
