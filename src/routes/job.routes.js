b9a9d6d2b6448e81ca83b22eca141c78a5280b94
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2025-12-01 16:02:58 +0200

    feat: add get jobs and get job routes

--- src/routes/job.routes.js
+++ src/routes/job.routes.js
@@ -18,11 +18,25 @@ router.put(
   (req, res, next) => jobController.updateJob(req, res, next),
 );
 
-router.put(
+router.delete(
   '/job/:companyId/delete/:jobId',
   Authorization.verifyToken,
   Authorization.verifyHRPermission,
   (req, res, next) => jobController.deleteJob(req, res, next),
+);
+
+router.get('/job', Authorization.verifyToken, (req, res, next) =>
+  jobController.getJobs(req, res, next),
+);
+
+router.get(
+  '/job/specific/:jobId',
+  Authorization.verifyToken,
+  (req, res, next) => jobController.getJob(req, res, next),
+);
+
+router.get('/job/:companyId', Authorization.verifyToken, (req, res, next) =>
+  jobController.getJobs(req, res, next),
 );
 
 export default router;
