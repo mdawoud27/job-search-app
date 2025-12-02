c9994abb92b6b34bb8efcf67603af0a13d6c6fa5
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2025-12-02 12:34:27 +0200

    feat: add create application route

--- src/routes/application.routes.js
+++ src/routes/application.routes.js
@@ -1,5 +1,18 @@
 import { Router } from 'express';
+import { applicationController } from '../container.js';
+import { Authorization } from '../middlewares/auth.middleware.js';
+import { uploadCV } from '../utils/multer.js';
 
 const router = Router();
+
+router.post(
+  '/jobs/:jobId/application',
+  Authorization.verifyToken,
+  Authorization.verifyUserPermission,
+  uploadCV.single('cv'),
+  (req, res, next) => {
+    applicationController.createApplication(req, res, next);
+  },
+);
 
 export default router;
