1ab2ab10c065102a5a15efbd09040815446669fd
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2025-12-02 12:30:14 +0200

    feat: add create application feature

--- src/controllers/application.controller.js
+++ src/controllers/application.controller.js
@@ -1,1 +1,25 @@
-export class ApplicationController {}
+import { uploadBuffer } from '../config/cloudinary.config.js';
+
+export class ApplicationController {
+  constructor(applicationService) {
+    this.applicationService = applicationService;
+  }
+
+  async createApplication(req, res, next) {
+    try {
+      if (!req.file) {
+        return next(new Error('No file uploaded'));
+      }
+
+      const cloudResult = await uploadBuffer(req.file.buffer, 'cv');
+      const application = await this.applicationService.createApplication(
+        req.user.id,
+        req.params.jobId,
+        cloudResult,
+      );
+      res.status(201).json(application);
+    } catch (error) {
+      next(error);
+    }
+  }
+}
