8b252d60fb5c1747ff731826205a4f36c974e667
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2025-12-02 12:35:18 +0200

    feat: add upload pdf files using multer

--- src/utils/multer.js
+++ src/utils/multer.js
@@ -12,8 +12,22 @@ function fileFilter(req, file, cb) {
   }
 }
 
-export const upload = multer({
+function fileFilterCV(req, file, cb) {
+  if (file.mimetype.startsWith('application/pdf')) {
+    cb(null, true);
+  } else {
+    cb(new Error('Only PDF are allowed'), false);
+  }
+}
+
+export const uploadImage = multer({
   storage,
   fileFilter,
+  limits: { fileSize: 5 * 1024 * 1024 }, // 5 MB max
+});
+
+export const uploadCV = multer({
+  storage,
+  fileFilter: fileFilterCV,
   limits: { fileSize: 5 * 1024 * 1024 }, // 5 MB max
 });
