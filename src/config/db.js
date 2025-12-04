a7e57a98e63a03cf75cc5c710e12eaa8710c410f
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2025-12-04 09:01:13 +0200

    feat: use mongodb atlas instead of localy db

--- src/config/db.js
+++ src/config/db.js
@@ -1,20 +1,19 @@
 import mongoose from 'mongoose';
 import * as dotenv from 'dotenv';
-// import { setupOTPCleanupJob } from '../utils/otpCleanup.js';
 import { startOTPCleanupJob } from '../jobs/otp-cleanup.job.js';
 dotenv.config();
 
 /* eslint no-undef: off */
 /* eslint no-console: off */
-const url = process.env.MONGO_URL;
-const dbName = process.env.DB_NAME;
+// const url = process.env.MONGO_URL;
+// const dbName = process.env.DB_NAME;
+const mongodb_url = process.env.MONGODB_URL; // mongodb atlas
 
 const connectToDB = async () => {
   try {
-    await mongoose.connect(`${url}/${dbName}`);
+    await mongoose.connect(`${mongodb_url}`);
     console.log(`Connected to DB`);
     startOTPCleanupJob();
-    // setupOTPCleanupJob();
   } catch (err) {
     console.log(`Database connection error: ${err.message}`);
     process.exit(1);
