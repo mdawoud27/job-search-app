801e5e1a518e8ec0af30a6575a65d498257b2c83
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2025-12-04 09:34:06 +0200

    refactor: error handler middleware

---
+++ src/middlewares/error.middleware.js
@@ -0,0 +1,13 @@
+export class ErrorHandler {
+  static notFound(req, res, next) {
+    const error = new Error(`NOT FOUND! - ${req.originalUrl}`);
+    res.status(404);
+    next(error);
+  }
+
+  static errorHandler(err, req, res, next) {
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+    res.status(statusCode).json({ message: err.message });
+    next();
+  }
+}
