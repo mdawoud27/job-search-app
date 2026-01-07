2842067cf7859c49b8de5de2dbe7ef6c8585e926
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2026-01-07 19:19:17 +0200

    feat: add company graphql schema

---
+++ src/graphql/schema/company.schema.js
@@ -0,0 +1,13 @@
+export const companySchema = `
+  type Company {
+    id: ID
+    companyName: String
+    description: String
+    industry: String
+    address: String
+    numberOfEmployees: String
+    companyEmail: String
+    logo: Image
+    coverPic: Image
+  }
+`;
